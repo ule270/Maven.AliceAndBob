45a8fa302cd1a824b616df3d65468cc36ca5c110
ule270 <u.le270@gmail.com>
2023-03-04 14:15:52 -0500

    Changes added.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args) {
+        String userName;
 
+        System.out.println("Please enter your name:");
+        Scanner keyboard = new Scanner(System.in);
+        userName = keyboard.nextLine();
+
+        if (userName.equals("Alice")) {
+            System.out.println("Hello " + userName);
+        }
+        if (userName.equals("Bob")) {
+            System.out.println("Hello " + userName);
+        }
     }
 }
